2f5f9a6a77fbe877f605f4ca0d5465eee1a65e77
Wes Belt <wes@diydev.io>
2024-08-07 11:31:19 -0400

    update backup

--- files/teslamate/teslamate-backup.sh
+++ files/teslamate/teslamate-backup.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
-rm tm.bck
-podman exec -t systemd-teslamate-database pg_dump -U teslamate teslamate > tm.bck
-tar -czf tm.tgz tm.bck
+BACKUPFILE="teslamate-database-backup.tgz"
+perform_backup()
+{
+  podman exec -t systemd-teslamate-database pg_dump -U teslamate teslamate | gzip -c > $BACKUPFILE
+}
+
+if [ -z $1 ]; then
+  if [ -f $BACKUPFILE ]; then
+    echo $0
+    echo ""
+    echo "Backup File ${1} already exists and over-write not provided!"
+    echo ""
+    echo "    select option"
+    echo ""
+    echo "      --over-write  - Overwrite backup file if it already exists"
+    echo ""
+  else
+    perform_backup
+  fi
+else
+  if [ "$1" == "--over-write" ]; then
+    if [ -f $BACKUPFILE ]; then
+      rm $BACKUPFILE
+    fi
+    perform_backup
+  else
+    echo $0
+    echo ""
+    echo "Unknown option {$1}"
+    echo ""
+    echo "    select option"
+    echo ""
+    echo "      --over-write  - Overwrite backup file if it already exists"
+    echo ""
+  fi
+fi
