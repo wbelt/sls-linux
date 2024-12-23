0aac687165eeac0d2a609f13a8b9ba9d7978a6d9
Wes Belt <wes@diydev.io>
2024-12-23 06:12:21 -0500

    update restore

--- files/teslamate/teslamate-restore.sh
+++ files/teslamate/teslamate-restore.sh
@@ -10,17 +10,10 @@ else
     systemctl --user stop teslamate-teslamate teslamate-grafana
     echo "Reset tables..."
     podman exec -i systemd-teslamate-database psql -U {{ pillar['teslamate']['database_user'] }} {{ pillar['teslamate']['database_name'] }} << .
-drop schema public cascade;
-create schema public;
-create extension cube;
-create extension earthdistance;
-CREATE OR REPLACE FUNCTION public.ll_to_earth(float8, float8)
-    RETURNS public.earth
-    LANGUAGE SQL
-    IMMUTABLE STRICT
-    PARALLEL SAFE
-    AS 'SELECT public.cube(public.cube(public.cube(public.earth()*cos(radians(\$1))*cos(radians(\$2))),public.earth()*cos(radians(\$1))*sin(radians(\$2))),public.earth()*sin(radians(\$1)))::public.earth';
-\q
+DROP SCHEMA public CASCADE;
+CREATE SCHEMA public;
+CREATE EXTENSION cube WITH SCHEMA public;
+CREATE EXTENSION earthdistance WITH SCHEMA public;
 .
     echo "Restore data..."
     gzip -dc $1 | podman exec -i systemd-teslamate-database psql -U {{ pillar['teslamate']['database_user'] }} -d {{ pillar['teslamate']['database_name'] }}
